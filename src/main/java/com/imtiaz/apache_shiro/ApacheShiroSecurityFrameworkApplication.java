ca213f947113ba4490d52df3694499c5b63747f2
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-03-16 22:37:52 +0600

    Initial commit

---
+++ src/main/java/com/imtiaz/apache_shiro/ApacheShiroSecurityFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.imtiaz.apache_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheShiroSecurityFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheShiroSecurityFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imtiaz/apache_shiro/ApacheShiroSecurityFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imtiaz.apache_shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheShiroSecurityFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
